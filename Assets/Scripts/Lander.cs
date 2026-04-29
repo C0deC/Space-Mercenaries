2c05ae7f9c6a639e2da3e72e7172a91c9b54a9ba
C0deC <62475219+C0deC@users.noreply.github.com>
2026-04-29 19:40:24 +0200

    Added Landing Pads and Multipliers Score to the landing

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -29,8 +29,40 @@ public class Lander : MonoBehaviour
         }
     }
 
-    private void Update()
+    private void OnCollisionEnter2D(Collision2D collision2D)
     {
+        if(!collision2D.gameObject.TryGetComponent(out LandingPad landingPad))
+        {
+            Debug.Log("Crashed on the Terrain!");
+            return;
+        }
 
+        float softLandingVelocityMagnitude = 4f;
+        float relativeVelocityMagnitude = collision2D.relativeVelocity.magnitude;
+        if(relativeVelocityMagnitude > softLandingVelocityMagnitude)
+        {
+            Debug.Log("Landed too hard!!");
+            return;
+        }
+
+        float dotVector = Vector2.Dot(Vector2.up, transform.up);
+        float minDotVector = 0.90f;
+        if(dotVector < minDotVector)
+        {
+            Debug.Log("Landed on a too steep angle!");
+            return;
+        }
+
+        Debug.Log("Succesful landing!!");
+
+        float maxScoreAmountLandingAngle = 100;
+        float scoreDotVectorMultiplier = 10f;
+        float landingAngleScore = maxScoreAmountLandingAngle - Mathf.Abs(dotVector - 1f) * scoreDotVectorMultiplier * maxScoreAmountLandingAngle;
+
+        float maxScoreAmountLandingSpeed = 100;
+        float landingSpeedScore = (softLandingVelocityMagnitude - relativeVelocityMagnitude) *  maxScoreAmountLandingSpeed;
+
+        Debug.Log("landingAngleScore: " +  landingAngleScore);
+        Debug.Log("landingSpeedScore: " +  landingSpeedScore);
     }
 }
